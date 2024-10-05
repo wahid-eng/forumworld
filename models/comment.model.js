7ca5a070722a231fc6584a697a7e1753d16c1f3b
Mohammad Wahid <mohammadwahid.eng@gmail.com>
2024-10-05 21:25:51 +0600

    models added

---
+++ models/comment.model.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const commentSchema = new mongoose.Schema(
+	{
+		content: { type: String, required: true },
+		author: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+			required: true,
+		},
+		post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
+	},
+	{ timestamps: true }
+);
+
+export default mongoose.model('Comment', commentSchema);
