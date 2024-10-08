57f454f33f7c7f007a85fe3ef2d2e8cc6956d3e8
Mohammad Wahid <mohammadwahid.eng@gmail.com>
2024-10-08 07:19:56 +0600

    concurrently added with frontend pages init

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Layout from './pages/Layout';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import Blogs from './pages/Blogs';
+import NoPage from './pages/NoPage';
 
 function App() {
-	return <h1>Hello world!</h1>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route index element={<Home />} />
+					<Route path="dashboard" element={<Dashboard />} />
+					<Route path="blogs" element={<Blogs />} />
+					<Route path="*" element={<NoPage />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
